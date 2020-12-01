b9cfd6199760b7d64d8450f9a594f7964e2002b3
dhruvi0102 <dhruvibavaria2@gmail.com>
2020-12-01 17:09:29 +0530

    Assignments

---
+++ SourceControlAssignment/ValidationExample/ValidationExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ValidationExample.MvcApplication" Language="C#" %>
---
+++ SourceControlAssignment/ValidationExample/ValidationExample/Models/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace ValidationExample.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "User Name")]
+        [StringLength(50, ErrorMessage = "Limit exceeded")]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "password Not Matched")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+
+        [Range(18, 60, ErrorMessage = "Please enter between 18 to 60")]
+        public int Age { get; set; }
+        [Required]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+    }
+}
