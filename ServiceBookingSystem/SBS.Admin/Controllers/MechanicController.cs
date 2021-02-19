b7a8e2761db373620db50cd5f290d3d9bb7ea7ca
Dhruvi Bavaria <dhruvibavaria2@gmail.com>
2021-02-19 19:51:19 +0530

    ServiceBookingSystem

--- ServiceBookingSystem/SBS.Admin/Controllers/MechanicController.cs
+++ ServiceBookingSystem/SBS.Admin/Controllers/MechanicController.cs
@@ -3,15 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SBS.Data;
+using System.Data.Entity;
 
 namespace SBS.Admin.Controllers
 {
     public class MechanicController : Controller
     {
         // GET: Mechanic
-        public ActionResult Index()
+        public class DealerController : Controller
         {
-            return View();
+            // GET: Dealer
+            public ActionResult Index()
+            {
+                return View();
+            }
+            public JsonResult GetMechanic(string sidx, string sort, int page, int rows)
+            {
+                ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+                sort = (sort == null) ? "" : sort;
+                int pageIndex = Convert.ToInt32(page) - 1;
+                int pageSize = rows;
+
+                var MechanicList = db.Mechanics.Select(
+                        t => new
+                        {
+                            t.ID,
+                            t.Name,
+                            t.MechanicNumber,
+                            t.Contact,
+                            t.ServiceID,
+                            t.EmailID,
+
+                        });
+                int totalRecords = MechanicList.Count();
+                var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+                if (sort.ToUpper() == "DESC")
+                {
+                    MechanicList = MechanicList.OrderByDescending(t => t.Name);
+                    MechanicList = MechanicList.Skip(pageIndex * pageSize).Take(pageSize);
+                }
+                else
+                {
+                    MechanicList = MechanicList.OrderBy(t => t.Name);
+                    MechanicList = MechanicList.Skip(pageIndex * pageSize).Take(pageSize);
+                }
+                var jsonData = new
+                {
+                    total = totalPages,
+                    page,
+                    records = totalRecords,
+                    rows = MechanicList
+                };
+                return Json(jsonData, JsonRequestBehavior.AllowGet);
+            }
+            [HttpPost]
+            public string Create([Bind(Exclude = "Id")] Mechanic Model)
+            {
+                ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+                string msg;
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        Model.Name = Guid.NewGuid().ToString();
+                        db.Mechanics.Add(Model);
+                        db.SaveChanges();
+                        msg = "Saved Successfully";
+                    }
+                    else
+                    {
+                        msg = "Validation data not successfully";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    msg = "Error occured:" + ex.Message;
+                }
+                return msg;
+            }
+
+            public string Edit(Mechanic Model)
+            {
+                ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+                string msg;
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        db.Entry(Model).State = EntityState.Modified;
+                        db.SaveChanges();
+                        msg = "Saved Successfully";
+                    }
+                    else
+                    {
+                        msg = "Validation data not successfully";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    msg = "Error occured:" + ex.Message;
+                }
+                return msg;
+            }
+            public string Delete(string Id)
+            {
+                ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+                Mechanic mechanic = db.Mechanics.Find(Id);
+                db.Mechanics.Remove(mechanic);
+                db.SaveChanges();
+                return "Deleted successfully";
+            }
         }
-    }
 }
--- ServiceBookingSystem/SBS.Admin/Controllers/ServiceController.cs
+++ ServiceBookingSystem/SBS.Admin/Controllers/ServiceController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SBS.Data;
+using System.Data.Entity;
 
 namespace SBS.Admin.Controllers
 {
@@ -12,6 +14,101 @@ namespace SBS.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetService(string sidx, string sort, int page, int rows)
+        {
+            ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+            sort = (sort == null) ? "" : sort;
+            int pageIndex = Convert.ToInt32(page) - 1;
+            int pageSize = rows;
+
+            var ServiceList = db.Services.Select(
+                    t => new
+                    {
+                        t.ID,
+                        t.ServiceName,
+                        t.Price,
+                        t.Duration,
+                        t.Active
+
+                    });
+            int totalRecords = ServiceList.Count();
+            var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+            if (sort.ToUpper() == "DESC")
+            {
+                ServiceList = ServiceList.OrderByDescending(t => t.ServiceName);
+                ServiceList = ServiceList.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            else
+            {
+                ServiceList = ServiceList.OrderBy(t => t.ServiceName);
+                ServiceList = ServiceList.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            var jsonData = new
+            {
+                total = totalPages,
+                page,
+                records = totalRecords,
+                rows = ServiceList
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public string Create([Bind(Exclude = "Id")] Service Model)
+        {
+            ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Model.ServiceName = Guid.NewGuid().ToString();
+                    db.Services.Add(Model);
+                    db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+
+        public string Edit(Service Model)
+        {
+            ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(Model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Delete(string Id)
+        {
+            ServiceBookingSystemEntities db = new ServiceBookingSystemEntities();
+           Service service = db.Services.Find(Id);
+            db.Services.Remove(service);
+            db.SaveChanges();
+            return "Deleted successfully";
         }
     }
 }
